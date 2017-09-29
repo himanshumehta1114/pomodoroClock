ccc5b30c1c36a736fbd70a336ad1b1db1ba998e2
himanshu <himanshumehta1114@gmail.com>
2017-09-29 23:34:54 +0530

    Added Timer

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 var brLen = 5;
 var sLen = 25;
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
 $(document).ready(function(){
 	$('#breakDisp').html(brLen);
 $('#breakDec').on('click',function(){
@@ -24,6 +41,9 @@ $('#sessionInc').on('click',function(){
 	sLen++;
 			$('#sessionDisp').html(sLen);		
 });
+$('#timeBox').on('click',function(){
+	startTimer(sLen*60, document.querySelector('#time'));	
+})
 
 });
 	
